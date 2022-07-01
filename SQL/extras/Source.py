f2035a0e5c6ab728dae59fb49091a5263284b1e6
nemo295 <105400471+nemo295@users.noreply.github.com>
2022-07-01 22:05:11 +0200

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**⌯⦙ كيفيه التنصيب :**")
+help2 = ("**⌯⦙ قـائمـه الاوامـر :**\n**⌯⦙ قنـاه السـورس :** @J0KER_7x\n**⌯⦙ شـرح اوامـر السـورس : @J0KER_7x**\n**⌯⦙ 𓆩 آݪــمــبــرمــج ݪــيدو 𓆪 : @J0KER7x ** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source TelethonJOKER7x: @J0KER_7x  𓇡.**"
